cac6e7f3a14f051b0e98bee9110150f5d49457ec
Max Bergqvist <max.bergqvist@student.nbi-handelsakademin.se>
2025-12-10 20:00:55 +0100

    handling enums in type aswell.
    if input doesnt match anny enum, return .badRequest

--- Service.cs
+++ Service.cs
@@ -6,6 +6,19 @@ static class Service
         staying,
         activities
     }
+
+     public enum ServiceType
+    {
+        hotel,
+        motel,
+        bedandbreakfast,
+        camping,
+        glamping,
+        museum,
+        training
+        
+    }
+    
     public record Post_Args(
         string Name,
         string Category,
@@ -16,24 +29,25 @@ static class Service
       {
             if(string.IsNullOrWhiteSpace(service.Name))
                 return Results.BadRequest($"Name is required");
-            
-
-            if(string.IsNullOrWhiteSpace(service.Type))
-                return Results.BadRequest($"Type is required");
 
             if(string.IsNullOrWhiteSpace(service.City))
                 return Results.BadRequest($"City is required");
 
+             if(!Enum.TryParse<ServiceType>(service.Type, ignoreCase: true, out var type))
+            {
+                return Results.BadRequest($"Invalid input: {service.Type}  type: hotel/motel/bedandbreakfast/camping/glamping");
+            }
+
             if(!Enum.TryParse<ServiceCategory>(service.Category, ignoreCase: true, out var category))
             {
-                return Results.BadRequest($"Invalid input: {service.Category}  category: staying/activities");
+                return Results.BadRequest($"Invalid input: {service.Category}  category: hotel/activities");
             }
             string query = "INSERT INTO service(name, category, type, city) VALUES(@name, @category, @type, @city )";
             var parameters = new MySqlParameter[]
             {
             new("@name", service.Name),
             new("@category", category.ToString()),
-            new("@type", service.Type),
+            new("@type", type.ToString()),
             new("@city", service.City)
             };
 
