1b96b62f0d655b9f92e7151391d7b11d4b42a560
OscarWEdu <oscar.ward@student.nbi-handelsakademin.se>
2025-12-11 14:09:01 +0100

    Add port management for non 3306 ports

--- Program.cs
+++ Program.cs
@@ -15,7 +15,8 @@ builder.Services.AddControllers();
 builder.Services.AddScoped<ILodgingRepository, LodgingRepository>();
 // End: Session koniguration, "in memory cache"
 
-Config config = new("server=127.0.0.1;uid=bookngo;pwd=bookngo;database=bookngo;");
+//CHANGE "port=*" to your active port before running, typically 3306
+Config config = new("server=127.0.0.1;port=3307;uid=bookngo;pwd=bookngo;database=bookngo;");
 builder.Services.AddSingleton(config);
 var app = builder.Build();
 
