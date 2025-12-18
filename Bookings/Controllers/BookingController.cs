ec05039439847ce55791c620da49bbf74f7987db
OscarWEdu <oscar.ward@student.nbi-handelsakademin.se>
2025-12-18 17:53:09 +0100

    Implement creating new bookings as a user
    Runs from the bookings endpoint. Creates a booking between the logged in user and the id of the lodging which is sent via the body

--- Bookings/Controllers/BookingController.cs
+++ Bookings/Controllers/BookingController.cs
@@ -8,7 +8,7 @@ namespace server.Bookings.Controllers;
 [Route("bookings")]
 public class BookingController(IBookingRepository _bookingRepository) : ControllerBase
 {
-    public record Post_Args(int UserID, int LodgingID);
+    public record Post_Args(int LodgingID);
 
     [HttpGet]
     public async Task<IEnumerable<Booking>> dbGetAllBookings() => await _bookingRepository.GetAllBookings();
@@ -16,4 +16,7 @@ public class BookingController(IBookingRepository _bookingRepository) : Controll
     [HttpGet]
     [Route("mine")]
     public async Task<IEnumerable<BookingInfo>> dbGetUserBookings() => await _bookingRepository.GetUserBookings(HttpContext);
+
+    [HttpPost]
+    public async Task dbCreateBooking(Post_Args lodging) => await _bookingRepository.CreateBooking(lodging, HttpContext);
 }
